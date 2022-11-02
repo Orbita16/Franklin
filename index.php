46e283a20784a9d8b94c1dc885207a51b68b96fd
Venya <Veny82014@gmail.com>
2022-11-02 23:48:47 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE php>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ben</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+
+        <form action="./form.php" method="post">
+            <input type="text" placeholder="Имя" name="name"/>
+            <!-- <input type="text" placeholder="Фамилия" name="surname"/>
+            <input type="text" placeholder="Почта" name="email"/>
+            <input type="password" placeholder="Пароль" name="password"/> -->
+                            
+            <input type="submit" value="Отправить">
+        </form>
+        
+    </body>
+</html>
